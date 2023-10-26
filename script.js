58dd1826ae700e6a33a6242dfbc56d0c69b7a59b
reebbal <106560008+reebbal@users.noreply.github.com>
2023-10-26 08:41:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var OwnerId = "424532716596494336"
+var OwnerId = " 946148998329278474"
 $.ajax({
-  url: "https://api.lanyard.rest/v1/users/" + OwnerId,
+  url: "api.lanyard.rest/v1/users/946148998329278474,
   type: "GET",
   success: function(res) {
     var data = res.data;
     var user_data = data.discord_user;
-    var avatar_img = "https://cdn.discordapp.com/avatars/" + OwnerId + "/" + user_data.avatar + ".gif?size=2048"
+    var avatar_img = "https://cdn.discordapp.com/attachments/983498280820432967/1165169494625353788/images_3.jpg?ex=6545dfa7&is=65336aa7&hm=ed579265393b117f2f08ba41c997cb775229b1d4e060ddc6a628ba9b43984ded& "/" + user_data.avatar + ".gif?size=2048"
     $(".avatar-img").attr("src", avatar_img)
     $(".discord-status").removeAttr("style")
     $(".discord-status").attr("class", $(".discord-status").attr("class") + " " + data.discord_status)
@@ -19,7 +19,7 @@ function connectToLink(url) {
 
 function copyText(txt) {
   navigator.clipboard.writeText(txt);
-  showMessage("클립보드에 복사되었습니다!");
+  showMessage("hello budd");
 };
 
 function showMessage(txt) {
